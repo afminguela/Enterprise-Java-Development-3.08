f0f09f912e5baf6e740bb8910486d733059a9221
afminguela <afminguela@gmail.com>
2025-07-31 12:06:06 +0200

    initial commit

---
+++ Nurses/src/main/java/com/afminguela/Nurses/NursesApplication.java
@@ -0,0 +1,13 @@
+package com.afminguela.Nurses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursesApplication.class, args);
+	}
+
+}
---
+++ Nurses/src/test/java/com/afminguela/Nurses/NursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afminguela.Nurses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
